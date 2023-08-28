8c72eac72dc264b4d70312d9aa3eca47ace2df23
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-08-28 21:04:24 +0300

    profile page structure

--- vite-react-pc-client/src/components/topbar/Topbar.jsx
+++ vite-react-pc-client/src/components/topbar/Topbar.jsx
@@ -3,42 +3,44 @@ import './topbar.css';
 import { Link } from 'react-router-dom';
 
 export default function Topbar() {
-  return (
-    <div className="topbar-container">
-        <div className="topbar-leftside">
-            <span className="logo"><i className="fas fa-mobile-alt"></i> Social Horizon</span>
-        </div>
-        <div className="topbar-center">
-            <div className="searchbar">
-                <i className="fas fa-search"></i>
-                <input type="text" placeholder="search"/>
+    return (
+        <div className="topbar-container">
+            <div className="topbar-leftside">
+                <span className="logo"><i className="fas fa-mobile-alt"></i> Social Horizon</span>
             </div>
-        </div>
-        <div className="topbar-rightside">
-            <div className="nav-icons">
-                <Link to="/login" className="icon-item">
-                    <i className="fas fa-sign-in-alt"></i>
-                </Link>
-                <Link to="/register" className="icon-item">
-                    <i className="fas fa-registered"></i>
-                </Link>
-                <Link to="/" className="icon-item">
-                    <i className="fas fa-newspaper"></i>
-                </Link>
-                <div className="icon-item">
-                    <i className="fas fa-envelope"></i>
-                    <p className="icon-badge">1</p>
-                </div>
-                <div className="icon-item">
-                    <i className="fas fa-bell"></i>
-                    <p className="icon-badge">2</p>
-                </div>
-                <div className="icon-item">
-                    <i className="fas fa-sign-out-alt"></i>
+            <div className="topbar-center">
+                <div className="searchbar">
+                    <i className="fas fa-search"></i>
+                    <input type="text" placeholder="search" />
                 </div>
             </div>
-            <img src="/assets/profile-pic.png" alt="profile-pic" />
+            <div className="topbar-rightside">
+                <div className="nav-icons">
+                    <Link to="/login" className="icon-item">
+                        <i className="fas fa-sign-in-alt"></i>
+                    </Link>
+                    <Link to="/register" className="icon-item">
+                        <i className="fas fa-registered"></i>
+                    </Link>
+                    <Link to="/" className="icon-item">
+                        <i className="fas fa-newspaper"></i>
+                    </Link>
+                    <div className="icon-item">
+                        <i className="fas fa-envelope"></i>
+                        <p className="icon-badge">1</p>
+                    </div>
+                    <div className="icon-item">
+                        <i className="fas fa-bell"></i>
+                        <p className="icon-badge">2</p>
+                    </div>
+                    <div className="icon-item">
+                        <i className="fas fa-sign-out-alt"></i>
+                    </div>
+                    <Link to='/profile' className="icon-item profile">
+                        <i className="fas fa-user-circle"></i>
+                    </Link>
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
--- vite-react-pc-client/src/pages/profile/Profile.jsx
+++ vite-react-pc-client/src/pages/profile/Profile.jsx
@@ -1,6 +1,17 @@
+import PostItem from '../../components/post-item/PostItem'
+import RightBar from '../../components/rightbar/RightBar'
+import './profile.css'
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <div className="profile-container">
+      <div className="profile-left-bar">
+        left
+      </div>
+
+      <div className="profile-right-bar">
+        <RightBar />
+      </div>
+    </div>
   )
 }
